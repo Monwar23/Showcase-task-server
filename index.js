511a38bae3421bec62eed3deebe61dead1fabf81
monwar23 <monwar2345@gmail.com>
2024-08-16 00:34:44 +0600

    basic setup completed

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('server is running')
+})
+app.listen(port, () => {
+    console.log(`server running : ${port}`);
+})
